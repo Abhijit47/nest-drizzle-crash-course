2de5d0e2399e571cf4caedb886b5e36dc6578f1c
Abhijit47 <akarmakar846@gmail.com>
2026-01-03 16:22:53 +0530

    02-Updated User Table and Added drizzle config, Added scripts.

--- src/drizzle/schemas.ts
+++ src/drizzle/schemas.ts
@@ -1,7 +1,8 @@
-import { integer, pgTable, serial, text } from 'drizzle-orm/pg-core';
-export const usersTable = pgTable('users_table', {
+import { pgTable, serial, text } from 'drizzle-orm/pg-core';
+
+export const users = pgTable('users', {
   id: serial('id').primaryKey(),
   name: text('name').notNull(),
-  age: integer('age').notNull(),
   email: text('email').notNull().unique(),
+  password: text('password').notNull(),
 });
